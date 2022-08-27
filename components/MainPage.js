0f6a24623e1380fe61a7fb98c609aa47111b4597
Mathurah Ravigulan <mathurah25@gmail.com>
2022-08-27 11:54:56 -0400

    Added image

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 export default function MainPage({ generatePrompt }) {
   const [story_id, setStory_id] = useState("");
@@ -18,6 +19,14 @@ export default function MainPage({ generatePrompt }) {
         <button onClick={generatePrompt} className={styles.bigButton}>
           start a story
         </button>
+
+        <div className={styles.imagePadding}>
+          <Image
+            src="/../public/magic-library.png"
+            width={500}
+            height={500}
+          ></Image>
+        </div>
 
         <p className={styles.description}>
           or... if you have a game code, enter it here
